c903e4efa3b9e51bff939a4bcd3896114a890bc4
Bryce Rothhammer <rothhammer.b@gmail.com>
2017-09-15 19:20:54 -0500

    add buttons based on user entry, next add logic so no blank buttons

--- assets/app.js
+++ assets/app.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
 
 	//Global Variables
-	
-
-
+	var addButton;
+	var userTopic;
 
 	//array used to populate buttons when the page loads
 	var topics = ["Running", "The Big Bang Theory", "Yoda", "Animals napping", "Cool bicycles", "Robin Willimas"];
 
 	//Function Calls
 	createButtons();
+	addTopic();
 
 	//Add user topics to topics array
-
+	function addTopic(){
+		$("#addUserTopic").on("click", function(){
+			userTopic = $("#topic-input").val().trim();
+			topics.push(userTopic);
+			createButtons();
+			return false;
+		});
+	}
+		
 	//Create buttons on page for each item in topics array
 	function createButtons(){
 		$("#gifButtons").empty();
 		for (var i = 0; i < topics.length; i++){
-			var addButton = $("<button>");
+			addButton = $("<button>");
 			addButton.addClass("btn btn-primary nextTopic");
 			addButton.attr("data-name", topics[i]);
 			addButton.text(topics[i]);
